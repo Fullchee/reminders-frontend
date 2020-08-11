8bf247a6f35e683276a5eff7d2207ad32dc4cd4a
Fullchee Zhang <fullchee@gmail.com>
2020-08-11 11:08:39 -0400

    Add the build folder

---
+++ build/precache-manifest.79ab0f2c06fc50e80ed94bfd9a32c01f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc13fcd0ba5380bf20f46d7bb838c81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35e5407cc020d90e1541",
+    "url": "/static/css/2.92cdbaba.chunk.css"
+  },
+  {
+    "revision": "480c1bd8f2545da06cca",
+    "url": "/static/css/main.cab8a479.chunk.css"
+  },
+  {
+    "revision": "35e5407cc020d90e1541",
+    "url": "/static/js/2.0067d425.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "/static/js/2.0067d425.chunk.js.LICENSE"
+  },
+  {
+    "revision": "480c1bd8f2545da06cca",
+    "url": "/static/js/main.dbef3246.chunk.js"
+  },
+  {
+    "revision": "7463b382ff2778293294",
+    "url": "/static/js/runtime-main.b7a31800.js"
+  },
+  {
+    "revision": "b8be32595c54daba88ea896f699ce126",
+    "url": "/static/media/add.b8be3259.svg"
+  },
+  {
+    "revision": "f1ad3b28a894ab6b83593e14e01f572b",
+    "url": "/static/media/overflow-menu--vertical.f1ad3b28.svg"
+  },
+  {
+    "revision": "897608654676461ce19a2ed5e040770a",
+    "url": "/static/media/renew.89760865.svg"
+  },
+  {
+    "revision": "9757a87884821564b03478aa97eb14db",
+    "url": "/static/media/search.9757a878.svg"
+  },
+  {
+    "revision": "c4a9197bd549614be0d46e953b5781fb",
+    "url": "/static/media/trash-can.c4a9197b.svg"
+  }
+]);
