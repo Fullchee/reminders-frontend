19ae9335bc3973d5ee2ae9960463706595317cfe
Fullchee Zhang <fullchee@gmail.com>
2021-06-20 23:09:39 -0400

    Add LoginForm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React from "react";
-import "./styles/App.scss";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Form from "./components/Form";
+import React from 'react';
+import './styles/App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Form from './components/Form';
 import history from './history';
-
+import LoginForm from './components/LoginForm';
 
 function App(props) {
   return (
     <Router history={history}>
       <Switch>
-        <Route
-          path="/link/:id"
-          render={({ match }) => <Form id={match.params.id} />}
-        />
-        <Route
-          path="/:id"
-          render={({ match }) => <Form id={match.params.id} />}
-        />
+        <Route path="/login" render={() => <LoginForm />} />
+        <Route path="/link/:id" render={({ match }) => <Form id={match.params.id} />} />
+        <Route path="/:id" render={({ match }) => <Form id={match.params.id} />} />
         <Route path="/" render={() => <Form id="0" />} />
       </Switch>
     </Router>
