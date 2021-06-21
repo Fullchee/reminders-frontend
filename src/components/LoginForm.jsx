543d0c80df7fa0e664859876a4d2e586aa29da24
Fullchee Zhang <fullchee@gmail.com>
2021-06-20 23:09:39 -0400

    Add LoginForm

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  state = {
+    username: '',
+    password: ''
+  };
+
+  handleLogin = (e, data) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/token-auth/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          username: json.user.username
+        });
+      });
+  };
+
+  handleChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevstate => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={e => this.handleLogin(e, this.state)}>
+        <h4>Log In</h4>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
