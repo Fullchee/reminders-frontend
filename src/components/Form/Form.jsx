da58053a6e2b89a9670fac27e61227a4a4b4a076
Fullchee Zhang <fullchee@gmail.com>
2021-08-20 17:13:24 -0400

    Ensure that it does go to the right id (found in the url)

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -77,6 +77,7 @@ export default class Form extends Component {
         iframe.contentDocument.body.addEventListener('keydown', disableCmdS);
       }
     }, 1000);
+    this.refresh(this.props.id);
   }
 
   componentDidUpdate(prevProps) {
