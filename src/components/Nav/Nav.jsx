97351b1c837e509b8ce4a4505e060baae34fc7ea
Fullchee Zhang <fullchee@gmail.com>
2021-08-28 22:57:36 -0400

    refactor: avoid the usage of 'props' keyword

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,30 +10,26 @@ import addIcon from '../../images/add.svg';
 import shuffle from '../../images/shuffle.svg';
 import './Nav.scss';
 
-function Nav(props) {
+function Nav({ refresh, clearForm, isFlagged, confirmDelete, toggleFlag }) {
   return (
     <nav className="buttons home-nav">
       <SearchBox />
       <div className="buttons__icons">
         <Tooltip title="Random" touchHold="true">
           <Ripple>
-            <button id="random" onClick={props.refresh}>
+            <button id="random" onClick={refresh}>
               <img src={shuffle} alt="random" />
             </button>
           </Ripple>
         </Tooltip>
         <Tooltip title="Add" touchHold="true">
           <Ripple>
-            <button id="add" onClick={props.clearForm}>
+            <button id="add" onClick={clearForm}>
               <img src={addIcon} alt="add" />
             </button>
           </Ripple>
         </Tooltip>
-        <NavMoreMenu
-          isFlagged={props.isFlagged}
-          onDelete={props.confirmDelete}
-          toggleFlag={props.toggleFlag}
-        />
+        <NavMoreMenu isFlagged={isFlagged} onDelete={confirmDelete} toggleFlag={toggleFlag} />
       </div>
     </nav>
   );
