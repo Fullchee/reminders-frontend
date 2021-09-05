651cb7eab4ae3d98c593e950b2110c2adce476f4
Fullchee Zhang <fullchee@gmail.com>
2021-09-05 10:05:59 -0400

    Update svg to be react-compatible

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -192,7 +192,7 @@ export function Form({ id }) {
       debugger;
       refresh(id);
     }
-  }, [id, link.id]);
+  }, [id]);
 
   return (
     <div className="app container">
@@ -232,7 +232,7 @@ export function Form({ id }) {
             >
               <p style={{ paddingRight: '4px' }}>URL </p>
               <svg
-                class="clipboard"
+                className="clipboard"
                 viewBox="0 0 16 16"
                 version="1.1"
                 width="16"
@@ -240,7 +240,7 @@ export function Form({ id }) {
                 aria-hidden="true"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M5.75 1a.75.75 0 00-.75.75v3c0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75v-3a.75.75 0 00-.75-.75h-4.5zm.75 3V2.5h3V4h-3zm-2.874-.467a.75.75 0 00-.752-1.298A1.75 1.75 0 002 3.75v9.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0014 13.25v-9.5a1.75 1.75 0 00-.874-1.515.75.75 0 10-.752 1.298.25.25 0 01.126.217v9.5a.25.25 0 01-.25.25h-8.5a.25.25 0 01-.25-.25v-9.5a.25.25 0 01.126-.217z"
                 ></path>
               </svg>
