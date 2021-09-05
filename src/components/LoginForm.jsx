86ae3667d0a30aa6a50b7f9404a5aff6ff9b7490
Fullchee Zhang <fullchee@gmail.com>
2021-09-05 15:48:23 -0400

    style: add styling to login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 class LoginForm extends React.Component {
   state = {
     username: '',
-    password: ''
+    password: '',
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    this.setState(prevstate => {
+    this.setState((prevstate) => {
       const newState = { ...prevstate };
       newState[name] = value;
       return newState;
@@ -18,24 +18,30 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={e => this.props.handleLogin(e, this.state)}>
-        <h4>Log In</h4>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <input type="submit" />
-      </form>
+      <div className="app container">
+        <div className="form-container">
+          <form onSubmit={(e) => this.props.handleLogin(e, this.state)}>
+            <h4>Log In</h4>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <button className="submit-button" type="submit">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
