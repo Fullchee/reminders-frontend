3b76f40864cc636559618186ac88fec9dc12497e
Fullchee Zhang <fullchee@gmail.com>
2021-12-31 22:42:04 -0500

    refactor: put speed controller in its own component

--- src/components/Form/PlayerManager/PlayerManager.jsx
+++ src/components/Form/PlayerManager/PlayerManager.jsx
@@ -1,27 +1,9 @@
 import PropTypes from "prop-types";
 import React from "react";
-import "./SpeedController.scss";
-export function PlayerManager(props) {
-  return (
-    <select
-      className="speed-controller"
-      onChange={(e) => {
-        props.onChange(parseFloat(e.target.value));
-      }}
-      value={props.speed}
-    >
-      <option value="0.5">0.5x</option>
-      <option value="0.9">0.9x</option>
-      <option value="1">1x</option>
-      <option value="1.25">1.25x</option>
-      <option value="1.5">1.5x</option>
-      <option value="1.75">1.75x</option>
-      <option value="2">2x</option>
-      <option value="2.25">2.25x</option>
-      <option value="2.5">2.5x</option>
-      <option value="2.75">2.75x</option>
-    </select>
-  );
+import { SpeedController } from "./SpeedController";
+import "./PlayerManager.scss";
+export function PlayerManager({ onChange, speed }) {
+  return <SpeedController onChange={onChange} speed={speed} />;
 }
 
 PlayerManager.propTypes = {
---
+++ src/components/Form/PlayerManager/SpeedController.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import React from "react";
+import "./SpeedController.scss";
+
+export function SpeedController(props) {
+  return (
+    <select
+      className="speed-controller"
+      onChange={(e) => {
+        props.onChange(parseFloat(e.target.value));
+      }}
+      value={props.speed}
+    >
+      <option value="0.5">0.5x</option>
+      <option value="0.9">0.9x</option>
+      <option value="1">1x</option>
+      <option value="1.25">1.25x</option>
+      <option value="1.5">1.5x</option>
+      <option value="1.75">1.75x</option>
+      <option value="2">2x</option>
+      <option value="2.25">2.25x</option>
+      <option value="2.5">2.5x</option>
+      <option value="2.75">2.75x</option>
+    </select>
+  );
+}
+
+SpeedController.propTypes = {
+  onChange: PropTypes.func,
+  speed: PropTypes.number,
+};
