78ebdf389e7e21931443674e8a7ea5e1b7e4f415
Fullchee Zhang <fullchee@gmail.com>
2022-04-23 14:04:25 -0400

    Skip forward in a youtube video by 10 secs or back by 5 secs

--- src/components/Form/VideoPlayer.jsx
+++ src/components/Form/VideoPlayer.jsx
@@ -2,20 +2,31 @@ import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import ReactPlayer from "react-player/youtube";
 import { SpeedController } from "./SpeedController/SpeedController";
+import "./VideoPlayer.scss";
 
 function VideoPlayer({ url }) {
   const [speed, setSpeed] = useState(1);
+  const [playing, setPlaying] = useState(false);
   const playerRef = useRef(null);
 
+  function moveForward({ seconds } = { seconds: 10 }) {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + seconds);
+    setPlaying(true);
+  }
   return (
     <>
       <ReactPlayer
+        playing={playing}
         ref={playerRef}
         playbackRate={speed}
         url={url}
         controls={true}
       />
       <SpeedController onChange={setSpeed} speed={speed} />
+      <div class="seek-buttons">
+        <button onClick={() => moveForward({ seconds: -5 })}>&lt;</button>
+        <button onClick={() => moveForward({ seconds: 10 })}>&gt;</button>
+      </div>
     </>
   );
 }
